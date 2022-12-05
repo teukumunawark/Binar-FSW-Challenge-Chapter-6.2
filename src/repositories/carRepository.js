3f9f7106cab4104ee285d29963f3386664ad462a
T. Munawar Khalil <tmunawar.k@mhs.unsyiah.ac.id>
2022-12-05 18:10:01 +0700

    update

--- src/repositories/carRepository.js
+++ src/repositories/carRepository.js
@@ -1,27 +1,49 @@
 const { Car } = require('../models');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 class CarRepository {
   static async create({
     plate, manufacture, model, rentPerDay, capacity,
     image, description, transmission, available,
-    type, year, availableAt
+    type, year, availableAt, userID
   }) {
-    const createCar = await Car.create({
-      plate, manufacture, model, rentPerDay, capacity,
-      image, description, transmission, available,
-      type, year, availableAt,
-    });
+    const createCar = await Car.create(
+      {
+        plate, manufacture, model, rentPerDay, capacity,
+        image, description, transmission, available,
+        type, year, availableAt
+      },
+      {
+        createdBy: userID,
+      }
+    );
+    console.log(`Repo ${userID}`);
+
     return createCar;
   }
 
   static async getAll() {
-    const getAllCars = await Car.findAll();
+    const getAllCars = await Car.findAll({
+      where: {
+        deletedAt: {
+          [Op.is]: null
+        }
+      },
+    });
 
     return getAllCars;
   }
 
   static async getById({ id }) {
-    const getCarById = await Car.findOne({ where: { id } });
+    const getCarById = await Car.findOne({
+      where: {
+        id,
+        deletedAt: {
+          [Op.is]: null
+        }
+      }
+    });
 
     return getCarById;
   }
@@ -29,19 +51,31 @@ class CarRepository {
   static async update({
     id, plate, manufacture, model, rentPerDay, capacity,
     image, description, transmission, available,
-    type, year, availableAt
+    type, year, availableAt, userID
   }) {
-    const updateCar = await Car.update({
-      plate, manufacture, model, rentPerDay, capacity,
-      image, description, transmission, available,
-      type, year, availableAt
-    }, { where: { id } });
+    const updateCar = await Car.update(
+      {
+        plate, manufacture, model, rentPerDay, capacity,
+        image, description, transmission, available,
+        type, year, availableAt
+      },
+      {
+        updatedBy: userID
+      },
+      { where: { id } },
+    );
 
     return updateCar;
   }
 
-  static async delete({ id }) {
-    const deletedCar = await Car.destroy({ where: { id } });
+  static async delete({ id, userID }) {
+    const deletedCar = await Car.update(
+      {
+        deletedAt: new Date().getTime(),
+        deletedBy: userID
+      },
+      { where: { id } }
+    );
 
     return deletedCar;
   }
